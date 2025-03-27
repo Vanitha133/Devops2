bc21396b4039bf3ac5a7500f64edaaa04a2b3ed7
Vanitha133 <vanithapurushothaman13@gmail.com>
2025-03-27 18:05:39 +0530

    adding python file

---
+++ Pythonjen1.py
@@ -0,0 +1,7 @@
+print("This program prints the sum of integers in the list")
+print("The list has numbers 1,2,3,4,5")
+sum=0
+numbers=[1,2,3,4,5]
+for i in range(len(numbers)):
+    sum=sum+numbers[i]
+print("Sum =",sum)
